b165b1cf65cfbff48ca5eb003085b55d16e93c64
Dibyaranjan Jena <45364252+dibyaranjanGIT@users.noreply.github.com>
2022-12-19 21:14:47 +0530

    Update

--- flask_blog/main.py
+++ flask_blog/main.py
@@ -1,5 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/cleanblog'
+db = SQLAlchemy(app)
+
+
+class Contact(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    email = db.Column(db.String(80))
+    msg = db.Column(db.String(120))
+    phone = db.Column(db.Integer)
 
 
 @app.route("/")
@@ -12,8 +25,18 @@ def about():
     return render_template('about.html')
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
+    if request.method == "POST":
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+
+        entry = Contact(name=name, phone = phone, msg = message, email = email )
+        db.session.add(entry)
+        db.session.commit()
+
     return render_template('contact.html')
 
 
