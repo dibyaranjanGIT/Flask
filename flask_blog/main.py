2d88bcb4a52625342a9eb16c08c1876e5736d5c3
Dibyaranjan Jena <45364252+dibyaranjanGIT@users.noreply.github.com>
2022-12-20 20:46:00 +0530

    update

--- flask_blog/main.py
+++ flask_blog/main.py
@@ -1,11 +1,29 @@
 from flask import Flask, render_template, request
 # Import sqlalchemy to connect to database
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
+import json
 
+
+local_server = True
+with open('config.json', mode='r') as f:
+    params = json.load(f)['params']
 
 app = Flask(__name__)
+app.config.update(
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = '465',
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = params['gmail_user'],
+    MAIL_PASSWORD = params['gmail_password']
+)
+mail = Mail(app)
+if local_server:
 # here configure your database url and db name
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/cleanblog'
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
 db = SQLAlchemy(app)
 
 # create a class to send the details to database table
@@ -38,8 +56,13 @@ def contact():
         entry = Contact(name=name, phone = phone, msg = message, email = email )
         db.session.add(entry)
         db.session.commit()
+        mail.send_message("New message form blog", 
+                            sender=email, 
+                            recipients=[params['gmail_user']],
+                            body = message + '\n' + phone
+                        )
 
-    return render_template('contact.html')
+    return render_template('contact.html', params=params)
 
 
 @app.route("/post")
